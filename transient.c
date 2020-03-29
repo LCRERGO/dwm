1dce36db09b8b4e668695ddfade15b4090201172
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2020-03-29 15:11:08 -0300

    Adding more watermarks for files

--- transient.c
+++ transient.c
@@ -1,3 +1,14 @@
+/*
+ *     _   __  _                __ 
+ *    / | / / (_)___  _________/ / 
+ *   /  |/ / / / __ \/ ___/ __  /  
+ *  / /|  / / / /_/ / /  / /_/ /   
+ * /_/ |_/_/ /\____/_/   \__,_/    
+ *      /___/                      
+ *       
+ * Author: Lucas Cruz dos Reis(L.C.R.) 
+ * Github: https://github.com/LCRERGO 
+*/
 /* cc transient.c -o transient -lX11 */
 
 #include <stdlib.h>
