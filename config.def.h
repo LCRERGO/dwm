bd142f86fa526e768300c9ab57e38471d90b14ad
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2020-06-13 09:50:11 -0300

    Removing a conflict entry

--- config.def.h
+++ config.def.h
@@ -122,8 +122,7 @@ static const char *filebrowser[]    = { termname, "-e", filebrowsername, NULL };
 static const char *musicplayer[]    = { termname, "-e", musicplayername, NULL };
 static const char *calendar[]       = { termname, "-e", calendarname, NULL };
 static const char *calculator[]     = { termname, "-e", calculatorname, NULL };
-static const char *socialclient[]   = { termname, "-e", irccname,
-    "--config=~/.config/irssi", "--home=~/.local/share/irssi", NULL };
+static const char *socialclient[]   = { termname, "-e", irccname, NULL };
 
 static Key keys[] = {
 	/* modifier                     key            function        argument */
